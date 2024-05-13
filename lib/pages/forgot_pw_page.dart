742611d5411d9da940abca36ce06ff517b61dd7b
NANDAKIAHORE GOWDA G <nandakishore01234@gmail.com>
2024-05-13 12:29:27 +0530

    Initial commit

---
+++ lib/pages/forgot_pw_page.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+class ForgotPassword extends StatefulWidget {
+  const ForgotPassword({Key? key}) : super(key: key);
+
+  @override
+  State<ForgotPassword> createState() => _ForgotPasswordState();
+}
+
+class _ForgotPasswordState extends State<ForgotPassword> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'Enter your Email and We Will send you a password reset link:',
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25.0),
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.grey[200],
+                  border: Border.all(color: Colors.white),
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(left: 20.0),
+                      child: TextField(
+                        decoration: InputDecoration(
+                          border: InputBorder.none,
+                          hintText: 'Email',
+                        ),
+                      ),
+                    ),
+
+                  ],
+                ),
+              ),
+            ),
+            MaterialButton(
+              onPressed: () {},
+              child: Text('Reset Password'),
+              color: Colors.orangeAccent,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
